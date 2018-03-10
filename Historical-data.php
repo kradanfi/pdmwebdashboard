88a9cc88b76ce9cb95c0d792f92b5cf2aa1dd4be
kradanfi <kunnachai@gmail.com>
2018-03-10 19:14:54 +0700

    debug

--- Historical-data.php
+++ Historical-data.php
@@ -3,7 +3,7 @@
       $display = "none";
       if(isset($_POST['select'])){
         print $_POST['date'];
-        $command = escapeshellcmd('python Python.py '.$_POST['date']);
+        $command = escapeshellcmd('python connect-hadoop.py '.$_POST['date']);
         $output = shell_exec($command);
         global $obj;
         $obj = json_decode($output);    
