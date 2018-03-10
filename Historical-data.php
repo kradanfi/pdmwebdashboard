e7e7c4af4efcbf4bb0e2168302c2e08633f62ae0
kradanfi <kunnachai@gmail.com>
2018-03-10 17:21:56 +0700

    debug

--- Historical-data.php
+++ Historical-data.php
@@ -7,7 +7,7 @@
         $output = shell_exec($command);
         global $obj;
         $obj = json_decode($output);    
-        print $output     
+        print $output ;    
         $display = "inline";
        }
        
